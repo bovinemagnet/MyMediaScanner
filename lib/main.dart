557649254a5619013c12681b2dff2c7a177f1e0a
Paul Snow <2058588+bovinemagnet@users.noreply.github.com>
2026-04-09 12:57:55 +1000

    fix: switch to just_audio_media_kit for desktop playback, add initialisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 import 'package:mymediascanner/app/app.dart';
 import 'package:mymediascanner/core/utils/window_manager_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  JustAudioMediaKit.ensureInitialized();
   await WindowManagerHelper.initialise();
   runApp(
     const ProviderScope(
